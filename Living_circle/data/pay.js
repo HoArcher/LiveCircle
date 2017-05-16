a82819a00a37e7180473cdd5e7c0ae74cf29cd94
HoHoo <elijah.hehao@gmail.com>
2017-05-17 01:02:05 +0800

    缴费

---
+++ Living_circle/data/pay.js
@@ -0,0 +1,18 @@
+var payDatas=[{
+	month:'2017-02',
+	shuidian:57.8,
+	tianranqi:15.0,
+	wuye:98.0
+},{
+	month:'2017-03',
+	shuidian:60.0,
+	tianranqi:15.0,
+	wuye:98.0
+},
+{
+	month:'2017-05',
+	shuidian:90.0,
+	tianranqi:15.0,
+	wuye:98.0
+}]
+　　
